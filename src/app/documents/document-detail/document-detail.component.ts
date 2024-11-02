7b2e689a1aa2b2ea8211655fb040f38e8f5e3300
ajrobbins44 <rob16003@byui.edu>
2024-11-02 13:49:42 -0700

    view btn works

--- src/app/documents/document-detail/document-detail.component.ts
+++ src/app/documents/document-detail/document-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Document } from '../document.model';
 import { DocumentService } from '../document.service';
+import { WindRefService } from '../../wind-ref.service';
 
 @Component({
   selector: 'cms-document-detail',
@@ -12,17 +13,25 @@ import { DocumentService } from '../document.service';
 })
 export class DocumentDetailComponent implements OnInit {
   document: Document;
+  nativeWindow: any;
   
   constructor(private documentService: DocumentService,
               private route: ActivatedRoute,
-              private router: Router) {}
+              private router: Router,
+              private windRefService: WindRefService) {}
   ngOnInit() {
     this.route.params
     .subscribe(
       (params: Params) => {
+        this.nativeWindow = this.windRefService.getNativeWindow()
         const id = params['id'];
         this.document = this.documentService.getDocument(id);
       }
     );
+  }
+  onView() {
+    if (this.document.url) {
+      this.nativeWindow.open(this.document.url);
+    }
   }
 }
