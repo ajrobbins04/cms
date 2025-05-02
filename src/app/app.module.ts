c4e1d3706c89de7577ade6c7a8decf524ec9c026
ajrobbins44 <rob16003@byui.edu>
2025-05-01 23:52:35 -0700

    image displays, but contact details still need to be added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactItemComponent } from './contacts/contact-list/contact-item/contact-item.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { DocumentListComponent } from './documents/document-list/document-list.component';
+import { DocumentItemComponent } from './documents/document-list/document-item/document-item.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactDetailComponent,
+    ContactListComponent,
+    ContactItemComponent,
+    DocumentsComponent,
+    DocumentListComponent,
+    DocumentItemComponent,
+    DocumentDetailComponent
   ],
   imports: [
     BrowserModule
