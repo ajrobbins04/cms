890133ad96ec31b7ac3ffc356974800c9fe52459
ajrobbins44 <rob16003@byui.edu>
2025-05-09 20:57:34 -0700

    improved document display

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
-  styleUrl: './document-list.component.css'
+  styleUrls: [
+    '../../app.component.css',
+    './document-list.component.css'
+  ]
 })
 export class DocumentListComponent {
 
