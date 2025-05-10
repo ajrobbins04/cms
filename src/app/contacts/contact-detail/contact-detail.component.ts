ea76fdb2c5c37d44bedb0be28ce569893e8ae364
ajrobbins44 <rob16003@byui.edu>
2025-05-09 18:08:07 -0700

    contact details display dynamically and new components added

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
   templateUrl: './contact-detail.component.html',
-  styleUrl: './contact-detail.component.css'
+  styleUrls: [
+    '../../app.component.css',    
+    './contact-detail.component.css'
+  ]
 })
 export class ContactDetailComponent implements OnInit{
-  contact: Contact;
+  @Input() contact: Contact;
 
   constructor() {}
 
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Contact } from '../contact.model';   
 
 
 @Component({
   selector: 'cms-contact-list',
   templateUrl: './contact-list.component.html',   
-  styleUrl: './contact-list.component.css'
+  styleUrls: [
+    '../../app.component.css',
+    './contact-list.component.css'
+  ]
 })
 export class ContactListComponent implements OnInit {
+  // The @Output decorator allows the component to emit events w/data to the parent component
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
+
   contacts: Contact[] = [
     new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', null),
     new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', null),
   ];
   constructor() {}
   ngOnInit() {}
+
+  // This method is called when a contact is selected in the contact-item component
+  onSelectedContact(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
+  }
 }
