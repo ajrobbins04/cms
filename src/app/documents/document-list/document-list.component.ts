64e03cb3803bdd87b2590ae697482607d2bb741a
ajrobbins44 <rob16003@byui.edu>
2025-05-13 08:55:04 -0700

    document list display and details display working

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -8,6 +9,23 @@ import { Component } from '@angular/core';
     './document-list.component.css'
   ]
 })
-export class DocumentListComponent {
+export class DocumentListComponent implements OnInit{
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document('1', 'Angular Guide', 'Comprehensive guide to Angular framework', 'https://angular.io/docs', null),
+    new Document('2', 'TypeScript Handbook', 'Official TypeScript documentation', 'https://www.typescriptlang.org/docs/', null),
+    new Document('3', 'JavaScript Basics', 'Introduction to JavaScript programming language', 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide', null),
+    new Document('4', 'Bootstrap 3 Documentation', 'Detailed documentation on Bootstrap 3', 'https://getbootstrap.com/docs/3.3/', null),
+    new Document('5', 'RxJS Guide', 'Guide to Reactive Extensions for JavaScript (RxJS)', 'https://rxjs.dev/guide/overview', null)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 
 }
