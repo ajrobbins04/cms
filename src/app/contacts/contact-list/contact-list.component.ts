6ddf71f97b0b3ba07d012066becd44479f7d251c
ajrobbins44 <rob16003@byui.edu>
2025-06-01 20:30:54 -0700

    contact service working as expected

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Contact } from '../contact.model';   
-
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-list',
@@ -11,18 +11,16 @@ import { Contact } from '../contact.model';
   ]
 })
 export class ContactListComponent implements OnInit {
-  // The @Output decorator allows the component to emit events w/data to the parent component
-  @Output() selectedContactEvent = new EventEmitter<Contact>();
+  @Output() contactSelectedEvent = new EventEmitter<Contact>();
 
-  contacts: Contact[] = [
-    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg', null),
-    new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg', null),
-  ];
-  constructor() {}
-  ngOnInit() {}
+  contacts: Contact[] = [];
+  constructor(private contactService: ContactService) {}
+  ngOnInit() {
+    this.contacts = this.contactService.getContacts().filter(contact => !contact.group);
+  }
 
   // This method is called when a contact is selected in the contact-item component
   onSelectedContact(contact: Contact) {
-    this.selectedContactEvent.emit(contact);
+    this.contactService.contactSelectedEvent.emit(contact);
   }
 }
--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Contact } from './contact.model';
 import {MOCKCONTACTS} from './MOCKCONTACTS';
 
@@ -7,8 +7,21 @@ import {MOCKCONTACTS} from './MOCKCONTACTS';
 })
 export class ContactService {
 
+  contactSelectedEvent = new EventEmitter<Contact>();
   contacts: Contact[] = [];
   constructor() { 
     this.contacts = MOCKCONTACTS;
+  }
+  getContacts(): Contact[] {
+    return this.contacts.slice(); // Return a copy of the contacts array
+  }
+
+  getContact(id: string): Contact {
+    for (let contact of this.contacts) {
+      if (contact.id === id) {
+        return contact; // Return the contact with the matching ID
+      }
+    }
+    return null; // Return null if no contact with the given ID is found
   }
 }
